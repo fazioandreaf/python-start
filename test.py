ff828e4a9fd8293ca53766720f0b481a7d9b1583
Fazioandrea.f <fazioandrea.f@gmail.com>
2022-01-03 16:20:24 +0100

    km converters miles

---
+++ test.py
@@ -0,0 +1,17 @@
+# print('the average is: ', (2+3)/5)
+# age = 10
+# print('My age is: ', age)
+# x = 9
+# y = 2323
+# print('the number are: ', x, '-', y)
+# number1 = input('Please eter the firt number: ')
+# number2 = input('Please eter the second number: ')
+# # concat
+# print(int(number1+number2))
+# # convertation
+# print(int(number1)+int(number2))
+
+# Km to miles Converter
+km = input('Please your kms: ')
+miles = round(float(km) * float(0.621371),3)
+print(miles, " miles")
